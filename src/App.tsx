96ed8a520a40c612d0f76d5680548c7cf49b8dae
WALI Ahmed <ahmed.wali@soprahr.com>
2021-02-28 17:53:52 +0100

    feat: integrate providers and routes into app racine

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { AuthProvider } from './context/AuthContext';
+import * as PropTypes from 'prop-types'
 
-function App() {
+function App({children}) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AuthProvider>
+        {children}
+      </AuthProvider>
     </div>
   );
 }
 
+App.propTypes = {
+  children: PropTypes.node
+}
+
 export default App;
+
+
