76641b8fefddde56c567923cee137c8efdd0bfa5
WALI Ahmed <ahmed.wali@soprahr.com>
2021-02-28 17:52:20 +0100

    feat: set up login/Home pages

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import { Form, Input, Button, Checkbox, Alert } from 'antd'
+import { UserOutlined, LockOutlined } from '@ant-design/icons'
+import { login } from '../../services/auth'
+import './login.scss'
+
+const Login = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const history = useHistory();
+  const push = history.push;
+  const onSubmit = values => {
+    setLoading(true)
+    login(values, { disableDefaultErrorMessage: true, push })
+      .then(() => {
+        setLoading(false)
+      }).catch(err => {
+      // setLoading(false)
+      setLoading(false)
+      setErrorMessage(err.message)
+    })
+  }
+
+  return (
+    <div className='login-background'>
+      <Form
+        name="normal_login"
+        className='login-form'
+        initialValues={{ remember: true }}
+        onFinish={onSubmit}
+      >
+        <Form.Item
+          name="email"
+          rules={[{ required: true, message: 'Veuillez saisir l\'adresse mail !' }]}
+        >
+          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: 'Veuillez saisir votre nom mot de passe!' }]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <a className="login-form-forgot" href="">
+            Forgot password
+          </a>
+        </Form.Item>
+
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" className="login-form-button" loading={loading}>
+            Log in
+          </Button>
+          {/*Or <a href="">register now!</a>*/}
+        </Form.Item>
+        {
+          errorMessage && <Alert message={errorMessage} type="error" showIcon />
+        }
+      </Form>
+    </div>
+  )
+}
+
+export default Login
